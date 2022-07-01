a05ca810e6bc321f4faf1a605b38f8eda1f8f6be
eetelber <enzo.etelbert@capgemini.com>
2022-07-01 12:26:57 +0200

    bug fix

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from explainerdashboard import ClassifierExplainer, ExplainerDashboard
 
 model = pickle.load(open('trained_model', 'rb'))
 data = pickle.load(open('test_data', 'rb'))
-X_test = data
+X_test, y_test = data
 
-explainer = ClassifierExplainer(model, X_test) #, y_test)
+explainer = ClassifierExplainer(model, X_test, y_test)
 # ExplainerDashboard(explainer).run()
 # ExplainerDashboard à réadapter
 db = ExplainerDashboard(explainer, title="Cool Title", shap_interaction=False)
--- training.py
+++ training.py
@@ -9,12 +9,15 @@ X = pd.read_csv("C:/projets/Openclassrooms/P7/App/export/train_X.csv", index_col
 y = pd.read_csv("C:/projets/Openclassrooms/P7/App/export/train_y.csv", index_col=0)
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
 
+y_train = y_train.squeeze()
+y_test = y_test.squeeze()
+
 model = LGBMClassifier(boosting='gbdt', learning_rate=0.1,
                        max_bin=510, num_leaves=16, objective='binary',
                        random_state=510, reg_alpha=1.2, reg_lambda=1.4,
                        subsample=0.7)
-y_train = y_train.squeeze()
+
 model.fit(X_train, y_train)
 
 pickle.dump(model, open('trained_model', 'wb'))
-pickle.dump((X_test), open('test_data', 'wb'))
+pickle.dump((X_test, y_test), open('test_data', 'wb'))
